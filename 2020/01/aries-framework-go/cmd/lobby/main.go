2de0f3867abfa1093c55eb201581a87e0ca4db11
Rolson Quadras <rolson.quadras@securekey.com>
2020-02-10 13:00:37 -0500

    feat: Dockerize Lobby Server
    - create docker image Lobby Server
    - add to docker compose
    
    Signed-off-by: Rolson Quadras <rolson.quadras@securekey.com>

--- 2020/01/aries-framework-go/cmd/lobby/main.go
+++ 2020/01/aries-framework-go/cmd/lobby/main.go
@@ -8,18 +8,28 @@ SPDX-License-Identifier: Apache-2.0
 package main
 
 import (
-	"log"
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 
+	"github.com/hyperledger/aries-framework-go/pkg/common/log"
 	"github.com/trustbloc/presentation/2020/01/aries-framework-go/cmd/lobby/api"
 	"github.com/trustbloc/presentation/2020/01/aries-framework-go/cmd/lobby/store/mem"
 )
 
+const addressPattern    = ":%s"
+var logger = log.New("aries-framework/lobby-server")
+
+
 func main() {
 	// get configs
-	addr := ":9071"
+	port := os.Getenv("LOBBY_SERVER_PORT")
+	if port == "" {
+		panic("port to be passed as ENV variable")
+	}
 
 	api := api.NewAPI(mem.New())
 
@@ -29,8 +39,8 @@ func main() {
 	r.HandleFunc("/demo/{uid}", api.GetDemo)
 	r.HandleFunc("/postInvitation", api.PostInvitation)
 
+	handler := cors.Default().Handler(r)
+
 	// start the server
-	log.Fatal(http.ListenAndServe(addr, r))
+	logger.Fatalf("lobby server start error %s", http.ListenAndServe(fmt.Sprintf(addressPattern, port), handler))
 }
-
-
